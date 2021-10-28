4257da102e59a308d8bd1e677ff3ee220950c69a
EwenQuim <ewen.quimerch@gmail.com>
2021-10-28 19:56:44 +0200

    Front - Copy to clipboard button

---
+++ static/copyToClipboard.js
@@ -0,0 +1,7 @@
+function copyToClipboard(element) {
+    /* Get the text field */
+    var copyText = document.getElementById(element);
+
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.innerHTML.trim());
+}
