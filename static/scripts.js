93c7f7ffdb8dc2d94ceb84bea10f547946dcf7a2
EwenQuim <ewen.quimerch@gmail.com>
2021-11-21 17:25:56 +0100

    characters count

--- static/scripts.js
+++ static/scripts.js
@@ -6,3 +6,15 @@ function copyToClipboard(element) {
     navigator.clipboard.writeText(copyText.innerHTML.trim());
 }
 
+function sendToClipboard(text) {
+    navigator.clipboard.writeText(text);
+}
+
+document.getElementById('message').onkeyup = function () {
+    var charCount = Math.max(0, 140 - document.getElementById('message').value.length);
+    if (charCount > 0) {
+        document.getElementById('countChars').innerHTML = charCount + " characters left before being able to send your message";
+    } else {
+        document.getElementById('countChars').innerHTML = "";
+    }
+};
