448ed2afe3458c573046e816042bbdd1b551c246
EwenQuim <ewen.quimerch@gmail.com>
2021-11-22 14:46:21 +0100

    generate small production CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  purge: [
-    "./templates/**/*.html"
-  ],
+  purge: {
+    enabled: ((process.env.ENV === 'production') ? true : false),
+    content: ["./templates/**/*.html"],
+  },
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
